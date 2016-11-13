ccd6fbfc52ed2fb794b82029ca3030d43543cd1b
simon <sim4nix@gamil.com>
2016-11-13 23:04:48 +0300

    Nginx config added

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+sudo rm -f /etc/nginx/sites-enabled/web_nginx.conf
 sudo ln -s /home/box/web/etc/web_nginx.conf  /etc/nginx/sites-enabled/web_nginx.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
