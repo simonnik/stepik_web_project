cd4143fa924ef2cca504089118e602e315e1e000
nesivi <trishichkin@mail.ru>
2016-11-16 01:41:40 +0300

    Задание 2

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo rm -f /etc/nginx/sites-enabled/web_nginx.conf
 sudo ln -s /home/box/web/etc/web_nginx.conf  /etc/nginx/sites-enabled/web_nginx.conf
 sudo /etc/init.d/nginx restart
-sudo rm -f /etc/gunicorn.d/hello.py
-sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo rm -f /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
