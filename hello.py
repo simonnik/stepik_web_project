bccda778cb24e0cd10a229d88cc33329e0a5829d
nesivi <trishichkin@mail.ru>
2016-11-16 02:20:44 +0300

    Задание 2

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def wsgi_application(environ, start_response):
     status = '200 OK'
-    headers = [('Content-Type', 'text-plain')]
+    headers = [('Content-Type', 'text/plain')]
     body = environ['QUERY_STRING'].split('&')
     body = [i+"\r\n" for i in body]
     start_response(status, headers)
