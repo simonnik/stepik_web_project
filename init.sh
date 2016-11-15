36cd7791521c9b7e8ad4347a7207777de1fb3566
nesivi <trishichkin@mail.ru>
2016-11-16 01:23:54 +0300

    Задание 2

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo rm -f /etc/nginx/sites-enabled/web_nginx.conf
 sudo ln -s /home/box/web/etc/web_nginx.conf  /etc/nginx/sites-enabled/web_nginx.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
