e14f623482cd93e11ba209a094c77be07a2cca01
nesivi <trishichkin@mail.ru>
2016-11-17 00:52:19 +0300

    Djando start

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo rm -f /etc/nginx/sites-enabled/web_nginx.conf
 sudo ln -s /home/box/web/etc/web_nginx.conf  /etc/nginx/sites-enabled/web_nginx.conf
 sudo /etc/init.d/nginx restart
