171a40527c771bcb643fb6a440280ba22a5dcfa8
nesivi <trishichkin@mail.ru>
2016-11-17 00:51:28 +0300

    Djando start

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@ sudo ln -s /home/box/web/etc/web_nginx.conf  /etc/nginx/sites-enabled/web_nginx.
 sudo /etc/init.d/nginx restart
 sudo rm -f /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo rm -f /etc/gunicorn.d/ask.py
+sudo ln -s /home/box/web/etc/ask.py   /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
