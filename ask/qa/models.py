92228adaf7a23c651074d2bc816d9c9249128eeb
nesivi <trishichkin@mail.ru>
2016-11-21 01:49:23 +0300

    Djando start

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,30 +1,25 @@
 from django.db import models
-from django.db import connection
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
-
-
 class QuestionManager(models.Manager):
-    def new():
-        return Question.objects.order_by('added_at')[:5]
-    def popular():
-        return Question.objects.order_by('rating')
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
 
 class Question(models.Model):
     objects = QuestionManager()
-    title = models.CharField(default="", max_length=255)
-    text = models.TextField(default="")
-    added_at = models.DateTimeField(blank=True)
+    title = models.CharField(max_length=1024)
+    text = models.TextField()
+    added_at = models.DateTimeField(default=datetime.now)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name="q_to_likes")
-
-    def __str__(self):
-        return self.title
+    author = models.ForeignKey(User, blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name='user-likes')
 
 class Answer(models.Model):
-    text = models.TextField(default="")
-    added_at = models.DateTimeField(blank=True)
-    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    text = models.TextField()
+    added_at = models.DateTimeField(default=datetime.now)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User, blank=True, null=True)
