211d4eba1425412ed06ffc90546de130dbdbdaff
nesivi <trishichkin@mail.ru>
2016-11-21 01:57:51 +0300

    Djando start

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ask',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'web',
+        'PASSWORD': 'web',
     }
 }
 
